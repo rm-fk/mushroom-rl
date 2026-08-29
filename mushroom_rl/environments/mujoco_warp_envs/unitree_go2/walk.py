83c5b43befb1132213c2437e3e45ef44bd529140
rm-fk <re4747gi-s@student.lu.se>
2026-08-30 05:02:07 +0700

    initial commit for go2 walk

---
+++ mushroom_rl/environments/mujoco_warp_envs/unitree_go2/walk.py
@@ -0,0 +1,23 @@
+import torch
+import warp as warp
+
+from mushroom_rl.environments.mujoco_warp import MuJoCoWarp
+from mushroom_rl.environments.mujoco import ObservationType
+
+class Go2Walk(MuJoCoWarp):
+    def __init__(
+            self,
+            )
+
+    super.__init__(
+            self,)
+
+    def is_absorbing():
+        pass
+
+
+
+
+
+
+
