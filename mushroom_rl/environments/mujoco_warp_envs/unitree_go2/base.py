00933a36a3369ad3a1ebbe46941aede421807309
rm-fk <re4747gi-s@student.lu.se>
2026-09-02 17:12:52 +0700

    initial commit

--- mushroom_rl/environments/mujoco_warp_envs/unitree_go2/base.py
+++ mushroom_rl/environments/mujoco_warp_envs/unitree_go2/base.py
@@ -6,24 +6,24 @@ from mushroom_rl.environments.mujoco_warp import MuJoCoWarp
 from mushroom_rl.environments.mujoco import ObservationType
 from mushroom_rl.core.spaces import Box
 
+
 class Go2Base(MuJoCoWarp):
     def __init__(
-            self,
-            num_envs,
-            gamma,
-            horizon, 
-            use_graph_capture,
-            ):
+        self,
+        num_envs,
+        gamma,
+        horizon,
+        use_graph_capture,
+    ):
         super().__init__(
-                num_envs=num_envs,
-                gamma=gamma,
-                horizon=horizon,
-                use_graph_capture=use_graph_capture.)
+            num_envs=num_envs,
+            gamma=gamma,
+            horizon=horizon,
+            use_graph_capture=use_graph_capture,
+        )
+
     def _preprocess_action():
         pass
 
     def _is_healthy():
         pass
-
-
-
