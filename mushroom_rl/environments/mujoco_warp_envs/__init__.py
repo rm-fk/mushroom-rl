4b94884f87206e1fff017106ce24b83008b04068
rm-fk <re4747gi-s@student.lu.se>
2026-09-07 00:20:15 +0700

    bugfix

--- mushroom_rl/environments/mujoco_warp_envs/__init__.py
+++ mushroom_rl/environments/mujoco_warp_envs/__init__.py
@@ -1,6 +1,6 @@
 from .locomotion import HopperWarp, Walker2DWarp, AntWarp, HalfCheetahWarp
 
-from go2 import Go2Base, Go2Walk
+from .go2 import Go2Base, Go2Walk
 
 Go2Base.register()
 Go2Walk.register()
